bf4de762c99ff0133acf27cafac5778cf3570057
Devianshi Goyal <goyalanshi22@gmail.com>
2026-03-17 13:53:06 -0700

    Initial Commit

---
+++ SkillSwap/SkillSwapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillSwapApp.swift
+//  SkillSwap
+//
+//  Created by Devianshi on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct SkillSwapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
